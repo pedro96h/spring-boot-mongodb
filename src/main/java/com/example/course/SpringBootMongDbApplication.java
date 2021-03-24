8f566e79f5cae7469023f89ad5fd0d96b9ce8d8d
Pedro Oliveira <pedro96h@gmail.com>
2021-03-24 09:06:22 -0300

    primeiro commit

---
+++ src/main/java/com/example/course/SpringBootMongDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongDbApplication.class, args);
+	}
+
+}
