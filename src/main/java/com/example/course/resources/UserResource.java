12335136bfd9f30e015b46c1899ec10070a953fb
Pedro Oliveira <pedro96h@gmail.com>
2021-03-24 09:23:32 -0300

    criação de classe user e camada rest

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.example.course.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		User u1 = new User("1", "Maria", "maria@gmail.com");
+		User u2 = new User("2", "Joao", "joao@gmail.com");
+		
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(u1,u2));
+		return ResponseEntity.ok().body(list);
+	}
+}
