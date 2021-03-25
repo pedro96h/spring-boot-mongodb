a1b775f7d56b7db554e9310ce9e55f442f38d715
Pedro Oliveira <pedro96h@gmail.com>
2021-03-25 08:12:45 -0300

    obtendo usuario por id , tratamento de exceção

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.course.domain.User;
 import com.example.course.repository.UserRepository;
+import com.example.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById (String id) {
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent()) {
+			return user.get();
+		}
+		throw new ObjectNotFoundException("Objeto não encontrador");
 	}
 }
