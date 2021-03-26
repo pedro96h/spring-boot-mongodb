09bf246bdf7302c7697c499446d3b0c18fa15867
Pedro Oliveira <pedro96h@gmail.com>
2021-03-26 00:21:10 -0300

    inserindo usuarios com post

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.course.domain.User;
+import com.example.course.dto.UserDTO;
 import com.example.course.repository.UserRepository;
 import com.example.course.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,13 @@ public class UserService {
 			return user.get();
 		}
 		throw new ObjectNotFoundException("Objeto não encontrador");
+	}
+	
+	public User insert (User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO (UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(),userDTO.getEmail());
 	}
 }
