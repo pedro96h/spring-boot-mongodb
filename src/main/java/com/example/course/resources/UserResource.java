d85ed3d288b34054994fccdeef16a7b9d6160c5b
Pedro Oliveira <pedro96h@gmail.com>
2021-03-26 08:40:47 -0300

    criação de endpoint para retorno de post de usuarios

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.course.domain.Post;
 import com.example.course.domain.User;
 import com.example.course.dto.UserDTO;
 import com.example.course.services.UserService;
@@ -62,5 +62,11 @@ public class UserResource {
 		user.setId(id);
 		user = userService.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts (@PathVariable String id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
