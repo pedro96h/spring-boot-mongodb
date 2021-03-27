d200fe14547162ebd4f008c15302c997ea11187a
Pedro Oliveira <pedro96h@gmail.com>
2021-03-27 17:53:55 -0300

    consulta simples com query methos

--- src/main/java/com/example/course/services/PostService.java
+++ src/main/java/com/example/course/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.course.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class PostService {
 			return post.get();
 		}
 		throw new ObjectNotFoundException("Objeto não encontrador");
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 
 }
