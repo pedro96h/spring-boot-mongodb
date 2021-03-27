fda118138a6e200ba15010da502cf86504f79296
Pedro Oliveira <pedro96h@gmail.com>
2021-03-26 23:11:53 -0300

    obtendo post por id'

---
+++ src/main/java/com/example/course/services/PostService.java
@@ -0,0 +1,26 @@
+package com.example.course.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.domain.Post;
+import com.example.course.repository.PostRepository;
+import com.example.course.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository; 
+	
+	public Post findById (String id) {
+		Optional<Post> post = postRepository.findById(id);
+		if(post.isPresent()) {
+			return post.get();
+		}
+		throw new ObjectNotFoundException("Objeto não encontrador");
+	}
+
+}
